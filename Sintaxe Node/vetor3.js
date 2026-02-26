7ecafd9ef527b5360b9cc8e40bf5d0f6b3b59833
GabrielOliveiraSilvac <gabriel.osilvaccor@gmail.com>
2026-02-26 16:37:31 -0300

    Delete Sintaxe Node directory

--- Sintaxe Node/vetor3.js
+++
@@ -1,8 +0,0 @@
-//Seção Três - Vetor de Busca
-
-//Declaração de Vetor
-const list = [10, 2, 4, 90, 3]
-
-//Laço For
-const numero = list.filter(list => list === 90);
-console.log("Numero Procurado: " +numero)
