98342b1df621c76051db6a9ec698c850350e0f11
Gabriel Oliveira Silva <gabriel.osilvaccor@gmail.com>
2026-09-19 16:47:13 -0300

    Update index.js

--- conexao_externa/index.js
+++ conexao_externa/index.js
@@ -9,6 +9,15 @@ import { buscarPedido } from './DAO/pedido/buscar_pedido.js'
 import { buscarPedidoProduto } from './DAO/pedido_produto/buscar_pedido-produto.js'
 import { viewEndereco } from './DAO/endereco/view_endereco.js'
 
+//Inserts
+import { incluirCliente } from './DAO/cliente/inserir_cliente.js'
+import { incluirEndereco } from './DAO/endereco/inserir_endereco.js'
+import { incluirLimite } from './DAO/limite_credito/inserir_limite.js'
+import { incluirPedido } from './DAO/pedido/inserir_pedido.js'
+import { incluirPedidoProduto } from './DAO/pedido_produto/inserir_pedido-produto.js'
+import { incluirProduto } from './DAO/produto/inserir_produto.js'
+
+
 const app = express()
 
 // Middleware obrigatório para o Express conseguir ler o corpo (body) das requisições em formato JSON
@@ -32,6 +41,17 @@ app.get('/cliente/:codigo', async (req, res) => {
     res.json(cliente)
 })
 
+//Incluir Cliente
+
+app.post('/clientes', async (req, res) => {
+    let {codigo, nome, sobreNome, cpf, telefone, id_limite, id_endereco} = req.body   // retirando dados do body da requisição
+    let infos = [codigo, nome, sobreNome, cpf, telefone, id_limite, id_endereco ]
+    let results = await incluirCliente(infos)
+
+    console.log(results)
+    res.json(results)
+})
+
 //Endereço Update
 app.get('/endereco', async (req, res) => {
     let endereco = await atualizarEndereco();
@@ -44,32 +64,79 @@ app.get('/enderecos', async (req, res) => {
     res.json(enderecos)
 })
 
-// Buscar Limite com o id do limite
-app.get('/limite/:id_limite', async (req, res) => {
-    const { id_limite } = req.params;
-    let limite = await buscarLimite(id_limite);
+//Incluir Endereço
+app.post('/enderecos', async (req, res) => {
+    let {id_endereco, logradouro, numero, cep, cidade} = req.body
+    let infos = [id_endereco, logradouro, numero, cep, cidade]
+    let results = await incluirEndereco(infos)
+
+    console.log(results)
+    res.json(results)
+})
+
+
+// Buscar Limite
+app.get('/limite', async (req, res) => {
+    let limite = await buscarLimite();
     res.json(limite)
 })
 
-//Procurando Produto com o preço abaixo de 500
+//Incluir Limite
+app.post('/limite', async (req, res) => {
+    let {id_limite, nome} = req.body
+    let infos = [id_limite, nome]
+    let results = await incluirLimite(infos)
+
+    console.log(results)
+    res.json(results)
+})
+
+//Procurando Produto
 app.get('/produto', async (req, res) => {
-    const { preco } = req.params;
-    let produto = await buscarProduto(preco);
+    let produto = await buscarProduto();
     res.json(produto)
 })
 
+//Incluir Produto
+app.post('/produto', async (req, res) => {
+    let {codigo, nome, descricao, preco} = req.body
+    let infos = [codigo, nome, descricao, preco]
+    let results = await incluirProduto(infos)
+
+    console.log(results)
+    res.json(results)
+})
+
 //Buscando Pedidos com o id do cliente
-app.get('/pedido/:id_cliente', async (req, res) => {
-    const { id_cliente } = req.params;
-    let pedido = await buscarPedido(id_cliente);
+app.get('/pedido', async (req, res) => {
+    let pedido = await buscarPedido();
     res.json(pedido)
 })
 
+//Incluir Pedido
+app.post('/pedido', async (req, res) => {
+    let {numero, data_elaboracao, id_cliente} = req.body
+    let infos = [numero, data_elaboracao, id_cliente]
+    let results = await incluirPedido(infos)
+
+    console.log(results)
+    res.json(results)
+})
+
 //Buscando na tabela Associativa de Pedido_Produto
-app.get('/Pedido_Produto/:id_pedido', async (req, res) => {
-    const { id_pedido } = req.params;
-    let associado = await buscarPedidoProduto(id_pedido);
+app.get('/Pedido_Produto', async (req, res) => {
+    let associado = await buscarPedidoProduto();
     res.json(associado)
+})
+
+//Incluir PedidoProduto
+app.post('/Pedido_Produto', async (req, res) => {
+    let {id_pedido, id_produto} = req.body
+    let infos = [id_pedido, id_produto]
+    let results = await incluirPedidoProduto(infos)
+
+    console.log(results)
+    res.json(results)
 })
 
 // Inicialização do Servidor
